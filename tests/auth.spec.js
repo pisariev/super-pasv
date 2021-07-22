52f06551181376438530f502aa2c8875ec2a2d4e
pisariev <ipisaryev@ukr.net>
2021-07-22 11:40:43 +0300

    add wrappers for api tests

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,27 +1,47 @@
 import { expect} from 'chai';
-import supertest from 'supertest';
+import AuthHelper from '../helpers/auth.helper';
 
 
 describe('auth', function () {
-    const request = supertest(process.env.BASE_URL);
+    const credentials = {
+        valid: {
+            login: process.env.LOGIN,
+            password: process.env.PASSWORD
+        },
+        invalid: {
+            login: 'invalid',
+            password: 'invalid'
+        }
+    }
 
-    it('successful log in', function (){
-        request
-            .post('/auth')
-            .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-            .end(function (err, res){
-                expect(res.statusCode).to.eq(200);
-                expect(res.body.token).not.to.be.undefined;
-            });
+    describe('successful log in', function () {
+        const authHelper = new AuthHelper();
+
+        before(async function () {
+           await authHelper.post(credentials.valid.login, credentials.valid.password)
+        });
+
+        it('responce status code is 200', function () {
+            expect(authHelper.response.statusCode).to.eq(200);
+        });
+
+        it('response body contains authorization token', function () {
+            expect(authHelper.response.body.token).not.to.be.undefined;
+        });
     });
+    describe('log in with wrong credentials should return error', function () {
+        const authHelper = new AuthHelper();
 
-    it('log in with wrong credentials should return error', function () {
-        request
-            .post('/auth')
-            .send({login: 'wrong', password: 'wrong'})
-            .end(function (err, res) {
-                expect(res.statusCode).to.eq(404);
-                expect(res.body.message).to.eq('Wrong login or password.');
-            });
-    })
+        before(async function () {
+            await authHelper.post(credentials.invalid.login, credentials.invalid.password)
+        });
+
+        it('response status code is 404', function() {
+            expect(authHelper.response.statusCode).to.eq(404);
+        });
+
+        it('response body contains error message', function(){
+            expect(authHelper.response.body.message).to.eq('Wrong login or password.');
+        });
+    });
 });
